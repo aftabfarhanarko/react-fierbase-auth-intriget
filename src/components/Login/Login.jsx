31adc47c53bfc42c556eabb5649822cf1cf7dd2f
Programming Hero <programming.hero1@gmail.com>
2025-10-12 16:09:25 -0500

    created based firebase react auth with user registration

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Login = () => {
+    return (
+        <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+                <h1 className="text-3xl font-bold">Please Login!</h1>
+                <form>
+                    <fieldset className="fieldset">
+                        {/* email field */}
+                        <label className="label">Email</label>
+                        <input type="email" name='email' className="input" placeholder="Email" />
+                        {/* password field */}
+                        <label className="label">Password</label>
+                        <input type="password" name='password' className="input" placeholder="Password" />
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <button className="btn btn-neutral mt-4">Login</button>
+                    </fieldset>
+                </form>
+                <p>New to our website? Please <Link className='text-blue-500 hover:text-blue-800' to="/register">Register</Link> </p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
