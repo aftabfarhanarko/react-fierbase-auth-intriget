782c01e20f768dba718d56b0da3fbbf1777b21ee
Programming Hero <programming.hero1@gmail.com>
2025-10-12 21:39:48 -0500

    sign in user using provider sign method

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
 
 const AuthProvider = ({ children }) => {
@@ -9,9 +9,26 @@ const AuthProvider = ({ children }) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    const signInUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // get current user info
+    onAuthStateChanged(auth, (currentUser) => {
+        if (currentUser) {
+            console.log('inside observer: if', currentUser)
+        }
+        else {
+            console.log('inside observer. else', currentUser)
+        }
+    })
+
+
+
     const authInfo = {
         // createUser: createUser
         createUser,
+        signInUser,
     }
 
     return (
